543682d3116ce9f8a9efc6fa16a7baa923ce58b0
Sonny Mattera <smattera@hotmail.com>
2025-03-19 23:48:23 -0500

    Integrate Tailwind CSS for styling; add global styles and update layout configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://msbandofchoctawindians.github.io',
-    base: 'cif-2025',
+  site: 'https://msbandofchoctawindians.github.io',
+  base: 'cif-2025',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
