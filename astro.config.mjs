51de277ec24055fa218aa4aff88df822aebb3a59
Sonny Mattera <smattera@hotmail.com>
2025-03-21 10:49:33 -0500

    Refactor layout and navigation; integrate React components and update 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://msbandofchoctawindians.github.io',
@@ -11,4 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
