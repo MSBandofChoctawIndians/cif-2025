9285c307358241cea5973180f608cb1e4bc363c1
Sonny Mattera <smattera@hotmail.com>
2025-03-22 15:10:59 -0500

    Add Save the Date component and update global styles; include new logo and Prettier configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://msbandofchoctawindians.github.io',
-  base: 'cif-2025',
+  site: "https://msbandofchoctawindians.github.io",
+  base: "cif-2025",
 
   vite: {
     plugins: [tailwindcss()],
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,66 +1,81 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/20/solid'
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { MapPinned, Menu, Ticket, X } from "lucide-react";
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+  { name: "Events", href: "#" },
+  { name: "Culture", href: "#" },
+  { name: "Family Fun", href: "#" },
+  { name: "Stickball", href: "#" },
+  { name: "Pageant", href: "#" },
+  { name: "Competitions", href: "#" },
+];
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+    <header className="bg-[rgb(33,37,41)] sticky top-0 z-1">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      >
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
-            />
+            <img alt="" src="/cif-2025/navlogo.png" className="h-12 w-auto" />
           </a>
         </div>
         <div className="flex lg:hidden">
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-300"
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
+            <Menu aria-hidden="true" className="size-6" />
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
+            <a
+              key={item.name}
+              href={item.href}
+              className="disabled text-sm/6 font-semibold text-gray-400 font-raleway"
+            >
               {item.name}
             </a>
-          ))}
-        </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
+            ))}
+            <a href="#" class="text-gray-400">
+              <MapPinned aria-hidden="true" className="mr-2 h-7 w-7 inline-block" />
+            </a>
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <button
+            type="button"
+            className="rounded-md bg-[var(--fair-gold)] px-3.5 py-2.5 text-sm font-semibold font-raleway shadow-sm hover:bg-[var(--fair-gold)]/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--fair-gold)]"
+          >
+            <Ticket
+              aria-hidden="true"
+              className="mr-2 h-5 w-5 inline-block mb-0.5"
+            />
+            Tickets
+          </button>
         </div>
       </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[rgb(33,37,41)] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
+              <img alt="" src="/cif-2025/navlogo.png" className="h-8 w-auto" />
             </a>
             <button
               type="button"
@@ -68,7 +83,7 @@ export default function Example() {
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
+              <X aria-hidden="true" className="size-6" />
             </button>
           </div>
           <div className="mt-6 flow-root">
@@ -78,24 +93,28 @@ export default function Example() {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-400 hover:bg-gray-200"
                   >
                     {item.name}
                   </a>
                 ))}
               </div>
               <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                <button
+                  type="button"
+                  className="rounded-md bg-[var(--fair-gold)] px-3.5 py-2.5 text-sm font-semibold font-raleway shadow-sm hover:bg-[var(--fair-gold)]/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--fair-gold)]"
                 >
-                  Log in
-                </a>
+                  <Ticket
+                    aria-hidden="true"
+                    className="mr-2 h-5 w-5 inline-block mb-0.5"
+                  />
+                  Tickets
+                </button>
               </div>
             </div>
           </div>
         </DialogPanel>
       </Dialog>
     </header>
-  )
+  );
 }
