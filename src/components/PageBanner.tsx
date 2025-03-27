0f119394612a79785d7d17a809178bd299dbb116
Sonny Mattera <smattera@hotmail.com>
2025-03-27 18:50:55 -0500

    Add alt text to PageBanner component for improved accessibility

--- src/components/PageBanner.tsx
+++ src/components/PageBanner.tsx
@@ -1,33 +1,32 @@
 import { motion } from "motion/react";
 
 interface PageBannerProps {
-    desktop: string;
-    mobile: string;
+  desktop: string;
+  mobile: string;
+  alt: string;
 }
 
-
-export default function PageBanner({ desktop, mobile }: PageBannerProps) {
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-            
-        >
-            <img
-                src={`/${desktop}.webp`}
-                width={1920}
-                height={1080}
-                alt="Events"
-                className="hidden md:block"
-            />
-            <img
-                src={`/${mobile}.webp`}
-                width={1080}
-                height={1920}
-                alt="Events"
-                className="block md:hidden"
-            />
-        </motion.div>
-    )
+export default function PageBanner({ desktop, mobile, alt }: PageBannerProps) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <img
+        src={`/${desktop}.webp`}
+        width={1920}
+        height={1080}
+        alt={alt}
+        className="hidden md:block"
+      />
+      <img
+        src={`/${mobile}.webp`}
+        width={1080}
+        height={1920}
+        alt={alt}
+        className="block md:hidden"
+      />
+    </motion.div>
+  );
 }
