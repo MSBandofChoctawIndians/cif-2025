f0f9dbb06206dfeb405db5164198a94ea3039f1d
Sonny Mattera <smattera@hotmail.com>
2025-03-27 19:28:25 -0500

    Update navigation overlay background and link hover styles for improved design

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -80,7 +80,7 @@ export default function Example() {
         onClose={setMobileMenuOpen}
         className="lg:hidden"
       >
-        <div className="fixed inset-0 z-50" />
+        <div className="fixed inset-0 z-0 bg-black/50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[rgb(33,37,41)] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
@@ -103,13 +103,13 @@ export default function Example() {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="-my-6 divide-y divide-[var(--fair-gold)]/50">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="font-roboto -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-200 hover:bg-gray-200"
+                    className="font-roboto -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-200 hover:bg-[var(--fair-gold)]/30"
                   >
                     {item.name}
                   </a>
