217054908ead9462fa92592871a8f2cb9a9eae2e
Sonny Mattera <smattera@hotmail.com>
2025-05-27 08:46:49 -0500

    feat: add schedule for July 01, 2025 with opening ceremonies details

--- src/data/schedule/day01.ts
+++ src/data/schedule/day01.ts
@@ -1,2 +1,9 @@
 // Tuesday, July 01, 2025
-export const day01 = [];
+export const day01 = [
+  {
+    name: "Opening Ceremonies & Fallen Tushka Ceremony",
+    time: "1:00 PM",
+    location: "Warrior Stadium",
+    category: "stickball",
+  },
+];
