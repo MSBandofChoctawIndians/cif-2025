a7574fc40d81bc40b4e88dbad9c0741fda29d7bd
Sonny Mattera <smattera@hotmail.com>
2025-06-12 01:15:46 -0500

    feat: Add Women's Bracket component and update main layout with SEO metadata
    - Created a new Women'sBracket component for displaying the women's division matches.
    - Updated main layout to include SEO metadata such as description, keywords, and Open Graph tags.
    - Modified stickball page to import the Women'sBracket component and commented out the rendering for future use.

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -11,29 +11,46 @@ import Autoplay from "embla-carousel-autoplay";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 export default function SponsorFooter() {
-  // Sample sponsor logos - replace with your actual sponsors
-  const sponsors = [
-    { id: 1, name: "Acme Inc", logo: "https://placehold.co/280x280" },
-    { id: 2, name: "TechCorp", logo: "https://placehold.co/280x280" },
+  const platSponsors = [
     {
-      id: 3,
-      name: "Innovate LLC",
-      logo: "https://placehold.co/280x280",
+      name: "Pearl River Resort",
+      logo: "/sponsors/platinum/pearlriver.png",
+      link: "https://www.pearlriverresort.com/",
     },
     {
-      id: 4,
-      name: "Future Systems",
-      logo: "https://placehold.co/280x280",
+      name: "First American Bank (FABI)",
+      logo: "/sponsors/platinum/fabi_white.png",
+      link: "https://www.fabicash.com/",
     },
     {
-      id: 5,
-      name: "Global Solutions",
-      logo: "https://placehold.co/280x280",
+      name: "The Yates Companies",
+      logo: "/sponsors/platinum/yates.png",
+      link: "https://www.wgyates.com/",
     },
     {
-      id: 6,
-      name: "NextGen Tech",
-      logo: "https://placehold.co/280x280",
+      name: "Pryor Morrow",
+      logo: "/sponsors/platinum/pryormorrow.png",
+      link: "https://pryormorrow.com/",
+    },
+    {
+      name: "Willis Engineering",
+      logo: "/sponsors/platinum/willisengineering.png",
+      link: "http://www.willisengineering.net/",
+    },
+    {
+      name: "Walt Schrimpf (SIS-UNUM)",
+      logo: "/sponsors/platinum/sis.png",
+      link: "https://www.unum.com/",
+    },
+    {
+      name: "Choctaw Health Center",
+      logo: "/sponsors/platinum/chc.png",
+      link: "https://www.choctawhealthcenter.org/",
+    },
+    {
+      name: "Trustmark",
+      logo: "/sponsors/platinum/trustmark.png",
+      link: "https://www.trustmark.com/",
     },
   ];
 
@@ -41,14 +58,13 @@ export default function SponsorFooter() {
     <footer className="via-fair-red w-full bg-gradient-to-br from-red-700 to-red-800 py-12">
       <div className="mb-12 mt-[-48px] h-48 bg-[url(/bead-pattern.svg)] bg-center bg-repeat-x opacity-30" />
       <div className="container mx-auto px-4 md:px-6">
-        {/* <div className="mb-8 text-center"> */}
-        <div className="text-center">
+        <div className="mb-8 text-center">
           <h2 className="text-3xl font-bold tracking-tight text-white">
             Thank You to Our 2025 Sponsors
           </h2>
         </div>
 
-        {/* <Carousel
+        <Carousel
           plugins={[Autoplay({ delay: 4000 })]}
           opts={{
             align: "start",
@@ -57,28 +73,34 @@ export default function SponsorFooter() {
           className="mx-auto w-full"
         >
           <CarouselContent>
-            {sponsors.map((sponsor) => (
+            {platSponsors.map((sponsor) => (
               <CarouselItem
-                key={sponsor.id}
+                key={sponsor.name}
                 className="pl-4 md:basis-1/2 lg:basis-1/3 2xl:basis-1/4"
               >
                 <div className="p-1">
-                  <Card className="border-2 border-white/50 bg-black/60 shadow-sm">
-                    <CardContent className="flex h-[280px] items-center justify-center p-6">
-                      <img
-                        src={sponsor.logo || "/placeholder.svg"}
-                        alt={`${sponsor.name} logo`}
-                        width={280}
-                        height={280}
-                        className="object-contain"
-                      />
-                    </CardContent>
-                    <CardFooter className="border-t p-4 py-0">
-                      <h3 className="w-full text-center text-sm font-medium text-white md:text-base lg:text-lg">
-                        Platinum Sponsor
-                      </h3>
-                    </CardFooter>
-                  </Card>
+                  <a
+                    href={sponsor.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Card className="border-2 border-white/50 bg-black/60 shadow-sm">
+                      <CardContent className="flex h-[280px] items-center justify-center p-6">
+                        <img
+                          src={sponsor.logo || "/placeholder.svg"}
+                          alt={`${sponsor.name} logo`}
+                          width={280}
+                          height={280}
+                          className="object-contain"
+                        />
+                      </CardContent>
+                      <CardFooter className="border-t p-4 py-0">
+                        <h3 className="w-full text-center text-sm font-medium text-white md:text-base lg:text-lg">
+                          Platinum Sponsor
+                        </h3>
+                      </CardFooter>
+                    </Card>
+                  </a>
                 </div>
               </CarouselItem>
             ))}
@@ -87,7 +109,7 @@ export default function SponsorFooter() {
             <CarouselPrevious className="relative mr-2" />
             <CarouselNext className="relative ml-2" />
           </div>
-        </Carousel> */}
+        </Carousel>
       </div>
     </footer>
   );
