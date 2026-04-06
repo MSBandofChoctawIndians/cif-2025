3692d589d096bdae810ecabd3fd389cb09c4cde5
Sonny Mattera <smattera@hotmail.com>
2026-04-06 16:03:31 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.choctawindianfair.com",
+  site: "/cif-2025/",
   vite: {
     plugins: [tailwindcss()],
   },
